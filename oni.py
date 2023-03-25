32d7fe169ed44fcd1e4cff7a5f5de1735a265bdf
KabilPreethamK <kabilpreethamk@gmail.com>
2023-03-25 07:28:36 -0400

    Slightly modified and Oni-net introduced

--- oni.py
+++ oni.py
@@ -30,8 +30,7 @@ class hack_auto:
         file_count = len(files)
 
         if file_count > 5:
-            cmd = input(
-                "Rc folder contains files more than 5. Do you want to clean the dictionary(recomended=yes) y or n ? ~~> ")
+            cmd = input("Rc folder contains files more than 5. Do you want to clean the dictionary(recomended=yes) y or n ? ~~> ")
             if "y" in cmd:
                 os.system("rm -f ./data/rc/*")
             if "n" in cmd:
@@ -44,8 +43,7 @@ class hack_auto:
         _, _, files = next(os.walk("./data/vulnerabilities/"))
         file_count = len(files)
         if file_count > 5:
-            cmd = input(
-                "Vulnerabilities folder contains files more than 5. Do you want to clean the dictionary(recomended=yes) y or n ? ~~> ")
+            cmd = input("Vulnerabilities folder contains files more than 5. Do you want to clean the dictionary(recomended=yes) y or n ? ~~> ")
             if "y" in cmd:
                 os.system("rm -r ./data/vulnerabilities/*")
 
@@ -59,8 +57,7 @@ class hack_auto:
         _, _, files = next(os.walk("./data/overall_vul/"))
         file_count = len(files)
         if file_count > 5:
-            cmd = input(
-                "Overall_vulnerabilities folder contains files more than 5. Do you want to clean the dictionary(recomended=yes) y or n ? ~~> ")
+            cmd = input("Overall_vulnerabilities folder contains files more than 5. Do you want to clean the dictionary(recomended=yes) y or n ? ~~> ")
             if "y" in cmd:
                 os.system("rm -r ./data/overall_vul/*")
 
@@ -70,8 +67,7 @@ class hack_auto:
             pass
 
     def target(self):
-        self.target_cmd = input(
-            "|O|N|I|[Hacking-Automatic]['What is the target ip']~~> ")
+        self.target_cmd = input("|O|N|I|[Hacking-Automatic]['What is the target ip']~~> ")
 
     def target_name(self):
         var = "|O|N|I|[Hacking-Automatic", self.target_cmd, "['What is the assesment name(eg: test)']~~> "
@@ -97,13 +93,11 @@ class hack_auto:
         with open("./data/vulnerabilities/"+self.actuall_folder+"_vulnerable.txt", "w") as create2:
             pass
 
-        response = os.system("ping -c 1 " + self.target_cmd +
-                             " > ./"+self.actuall_folder+"/Ping.txt")
+        response = os.system("ping -c 1 " + self.target_cmd + " > ./"+self.actuall_folder+"/Ping.txt")
         if response == 0:
             print("Network Active")
             print("Proceeding layer scan with nmap.. This may take time..")
-            os.system("nmap -sV "+self.target_cmd+" > ./" +
-                      self.actuall_folder+"/Nmap_Result")
+            os.system("nmap -sV "+self.target_cmd+" > ./" +self.actuall_folder+"/Nmap_Result")
             sep_content1 = []
             with open("./"+self.actuall_folder+"/Nmap_Result", "r") as f:
                 content = f.readlines()
@@ -117,69 +111,53 @@ class hack_auto:
                     if ports == "80/tcp":
                         print("Http is open.")
                         with open("./data/rc/"+self.actuall_folder+"_msf.rc", "a") as g:
-                            line1 = g.writelines(
-                                "use auxiliary/scanner/http/http_version \n")
-                            line2 = g.writelines(
-                                "set rhosts "+self.target_cmd+"\n")
+                            line1 = g.writelines("use auxiliary/scanner/http/http_version \n")
+                            line2 = g.writelines("set rhosts "+self.target_cmd+"\n")
                             line3 = g.writelines("run\n")
 
                     if ports == "21/tcp":
                         print("Ftp is open.")
                         with open("./data/rc/"+self.actuall_folder+"_msf.rc", "a") as g:
-                            line4 = g.writelines(
-                                "use auxiliary/scanner/ftp/ftp_version \n")
-                            line5 = g.writelines(
-                                "set rhosts "+self.target_cmd+"\n")
+                            line4 = g.writelines("use auxiliary/scanner/ftp/ftp_version \n")
+                            line5 = g.writelines("set rhosts "+self.target_cmd+"\n")
                             line6 = g.writelines("run\n")
 
                     if ports == "22/tcp":
                         print("ssh is open.")
                         with open("./data/rc/"+self.actuall_folder+"_msf.rc", "a") as g:
-                            line4 = g.writelines(
-                                "use auxiliary/scanner/ssh/ssh_version \n")
-                            line5 = g.writelines(
-                                "set rhosts "+self.target_cmd+"\n")
+                            line4 = g.writelines("use auxiliary/scanner/ssh/ssh_version \n")
+                            line5 = g.writelines("set rhosts "+self.target_cmd+"\n")
                             line6 = g.writelines("run\n")
 
                     if ports == "23/tcp":
                         print("Telnet is open.")
                         with open("./data/rc/"+self.actuall_folder+"_msf.rc", "a") as g:
-                            line4 = g.writelines(
-                                "use auxiliary/scanner/telnet/telnet_version \n")
-                            line5 = g.writelines(
-                                "set rhosts "+self.target_cmd+"\n")
+                            line4 = g.writelines("use auxiliary/scanner/telnet/telnet_version \n")
+                            line5 = g.writelines("set rhosts "+self.target_cmd+"\n")
                             line6 = g.writelines("run\n")
 
                     if ports == "3306/tcp":
                         print("Mysql is running and port is open.")
                         with open("./data/rc/"+self.actuall_folder+"_msf.rc", "a") as g:
-                            line4 = g.writelines(
-                                "use auxiliary/scanner/mysql/mysql_login \n")
-                            line5 = g.writelines(
-                                "set rhosts "+self.target_cmd+"\n")
+                            line4 = g.writelines("use auxiliary/scanner/mysql/mysql_login \n")
+                            line5 = g.writelines("set rhosts "+self.target_cmd+"\n")
                             line6 = g.writelines("run\n")
 
                 with open("./data/rc/"+self.actuall_folder+"_msf.rc", "a") as g:
                     line1 = g.writelines("exit")
 
-                os.system("msfconsole -r ./data/rc/"+self.actuall_folder +
-                          "_msf.rc -o ./data/vulnerabilities/"+self.actuall_folder+"_vulnerable.txt")
-                separate_vul_1("./data/vulnerabilities/" +
-                               self.actuall_folder+"_vulnerable.txt")
+                os.system("msfconsole -r ./data/rc/"+self.actuall_folder +"_msf.rc -o ./data/vulnerabilities/"+self.actuall_folder+"_vulnerable.txt")
+                separate_vul_1("./data/vulnerabilities/" +self.actuall_folder+"_vulnerable.txt")
                 separate_vul_2()
-                overall("./data/overall_vul/"+self.actuall_folder +
-                        "_auxiliary_scan_overall.txt")
+                overall("./data/overall_vul/"+self.actuall_folder +"_auxiliary_scan_overall.txt")
                 try:
                     gobust(self.target_cmd, self.actuall_folder)
                 except:
                     print("Gobuster cannot run on this IP.")
                 os.system("mkdir "+self.actuall_folder+"/Gatherings/")
-                os.system("cp ./data/overall_vul/" + self.actuall_folder +
-                          "_auxiliary_scan_overall.txt "+self.actuall_folder+"/Gatherings/")
-                os.system("cp ./data/gobuster_"+self.actuall_folder +
-                          ".txt "+self.actuall_folder+"/Gatherings/")
-                os.system("cp ./data/rc/"+self.actuall_folder +
-                          "_msf.rc "+self.actuall_folder+"/Gatherings/")
+                os.system("cp ./data/overall_vul/" + self.actuall_folder +"_auxiliary_scan_overall.txt "+self.actuall_folder+"/Gatherings/")
+                os.system("cp ./data/gobuster_"+self.actuall_folder +".txt "+self.actuall_folder+"/Gatherings/")
+                os.system("cp ./data/rc/"+self.actuall_folder +"_msf.rc "+self.actuall_folder+"/Gatherings/")
                 os.system("cp -r ./data/separate_1 "+self.actuall_folder)
                 os.system("cp -r ./data/separate_2 "+self.actuall_folder)
 
@@ -197,26 +175,16 @@ class finall_Report(hack_auto):
     def report(self):
         #try:
             with open("./Reports/Report_"+self.actuall_folder+".txt", "w") as f, open("./"+self.actuall_folder+"/Target.txt", "r") as g, open("./"+self.actuall_folder+"/Nmap_Result", "r") as h, open("./"+self.actuall_folder+"/Ping.txt", "r") as i, open("./"+self.actuall_folder+"/Gatherings/"+self.actuall_folder+"_auxiliary_scan_overall.txt", "r") as j:
-                l1 = f.write(
-                    ".oPYo. o    o o        .oPYo.                            \n")
-                l2 = f.write(
-                    "8    8 8b   8 8        8   `8                            \n")
-                l2 = f.write(
-                    "8    8 8`b  8 8       o8YooP' .oPYo. .oPYo. .oPYo. odYo. \n")
-                l2 = f.write(
-                    "8    8 8 `b 8 8 ooooo  8   `b 8oooo8 8    ' 8    8 8' `8 \n")
-                l2 = f.write(
-                    "8    8 8  `b8 8        8    8 8.     8    . 8    8 8   8 \n")
-                l2 = f.write(
-                    "`YooP' 8   `8 8        8    8 `Yooo' `YooP' `YooP' 8   8 \n")
-                l2 = f.write(
-                    ":.....:..:::....:::::::..:::..:.....::.....::.....:..::..\n")
-                l2 = f.write(
-                    ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n")
-                l2 = f.write(
-                    ":::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n\n\n")
-                l2 = f.write(
-                    "       Internet Protocol Adress of the target:\n\n")
+                l1 = f.write(".oPYo. o    o o        .oPYo.                            \n")
+                l2 = f.write("8    8 8b   8 8        8   `8                            \n")
+                l2 = f.write("8    8 8`b  8 8       o8YooP' .oPYo. .oPYo. .oPYo. odYo. \n")
+                l2 = f.write("8    8 8 `b 8 8 ooooo  8   `b 8oooo8 8    ' 8    8 8' `8 \n")
+                l2 = f.write("8    8 8  `b8 8        8    8 8.     8    . 8    8 8   8 \n")
+                l2 = f.write("`YooP' 8   `8 8        8    8 `Yooo' `YooP' `YooP' 8   8 \n")
+                l2 = f.write(":.....:..:::....:::::::..:::..:.....::.....::.....:..::..\n")
+                l2 = f.write(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n")
+                l2 = f.write(":::::::::::::::::::::::::::::::::::::::::::::::::::::::::\n\n\n")
+                l2 = f.write("       Internet Protocol Adress of the target:\n\n")
                 for lines in g:
                     f.write(lines)
 
@@ -264,8 +232,7 @@ class hacking:
             if "exit" in self.menu_cmd:
                 break
             if "options" in self.menu_cmd:
-                print(
-                    "1-Automatic reconnaissance  -------> Assessment held by ONI---- To use type 'recon'")
+                print("1-Automatic reconnaissance  -------> Assessment held by ONI---- To use type 'recon'")
             if "recon" in self.menu_cmd:
                 x = finall_Report()
                 x.check_sep1()
@@ -287,22 +254,14 @@ class main:
         pass
 
     def display(self):
-        print(
-            " _______  _       _________     _______  _______  _______  _______  _       ")
-        print(
-            "(  ___  )( (    /|\__   __/    (  ____ )(  ____ \(  ____ \(  ___  )( (    /|")
-        print(
-            "| (   ) ||  \  ( |   ) (       | (    )|| (    \/| (    \/| (   ) ||  \  ( |")
-        print(
-            "| |   | ||   \ | |   | | _____ | (____)|| (__    | |      | |   | ||   \ | |")
-        print(
-            "| |   | || (\ \) |   | |(_____)|     __)|  __)   | |      | |   | || (\ \) |")
-        print(
-            "| |   | || | \   |   | |       | (\ (   | (      | |      | |   | || | \   |")
-        print(
-            "| (___) || )  \  |___) (___    | ) \ \__| (____/\| (____/\| (___) || )  \  |")
-        print(
-            "(_______)|/    )_)\_______/    |/   \__/(_______/(_______/(_______)|/    )_)")
+        print(" _______  _       _________     _______  _______  _______  _______  _       ")
+        print("(  ___  )( (    /|\__   __/    (  ____ )(  ____ \(  ____ \(  ___  )( (    /|")
+        print("| (   ) ||  \  ( |   ) (       | (    )|| (    \/| (    \/| (   ) ||  \  ( |")
+        print("| |   | ||   \ | |   | | _____ | (____)|| (__    | |      | |   | ||   \ | |")
+        print("| |   | || (\ \) |   | |(_____)|     __)|  __)   | |      | |   | || (\ \) |")
+        print("| |   | || | \   |   | |       | (\ (   | (      | |      | |   | || | \   |")
+        print("| (___) || )  \  |___) (___    | ) \ \__| (____/\| (____/\| (___) || )  \  |")
+        print("(_______)|/    )_)\_______/    |/   \__/(_______/(_______/(_______)|/    )_)")
         print("\n")
         print("                  Developed by - KABIL PREETHAM K \n")
         print("         Git Hub :- https://github.com/KabilPreethamK \n")
