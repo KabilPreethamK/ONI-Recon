16e63025e77d2daf74f4dbf9aec94b5c4bb64854
H-Hacker <oni@localhost.localdomain>
2023-04-19 23:56:10 -0400

    Thursday

--- oni-net.py
+++ oni-net.py
@@ -1,4 +1,19 @@
 class summary_manual:
+
+    def auxiliary(self):
+        with open("./Assessments/msfadmin/Gatherings/msfadmin_auxiliary_scan_overall.txt","r") as f:
+            
+            content= f.readlines()
+            length = len(content)
+            for i in range(1, length):
+                #print(content[i])
+                grab = content[i]
+                if "esa11" in grab:
+                    grab_final=content[i+1].split(" ")
+                    join = grab_final[2:]
+                    print(" ".join(join))
+
+
     def open_file(self):
         with open("./Assessments/family/Ping.txt","r") as ping:
             self.cont=ping.readlines()
@@ -31,7 +46,14 @@ class summary_manual:
                 #self.edited_version = "[+]"+version
                 #self.lists2.append(self.edited_version)
                 #print(self.lists2)
-    
+    def nmap(self):
+        with open("./Assessments/msfadmin/Nmap_Result","r") as f:
+            lists = []
+            cont = f.readlines()
+            content = [x.split() for x in cont]
+            for i in range(5,len(cont)-4):
+                scan=cont[i].replace(cont[i][0:15]," ")
+                lists.append(scan)    
     
     
         
@@ -41,15 +63,25 @@ class summary_manual:
             cont = f.write("$From the above reconnaissance done by ONI it is declared that the target is responding and sending back the packets in the time of "+self.final_res+" milliseconds. This shows that the firewall is not blocking our ping. \n")
             cont = f.write("On running nmap the obtained open ports are:\n\n")
             count = 0
+            count1 = 0
             for ports in self.lists:
                 count = count+1
                 l=str(ports)+"\n".join(" ")
                 cont = f.writelines(l)
-            cont  = f.writelines("\n\n$These are the open ports that are running behind the target,the versions of the ports running in the backgrounds are separately scratched and saved as in map results inside the folder of assessments of the given Target name (location : ./Assessments/yourtargetname/Nmap_Result) \n")
-            cont = f.writelines("\n$But for further informations oni uses auxiliary scanners with the help of metasploitable toolkit , so that we can obtain the versions of the ports\n")
+            cont  = f.writelines("\n\n$These are the open ports that are running behind the target,the versions of the ports running in the backgrounds are separately scratched and saved as in map results inside the folder of assessments of the given Target name (location : ./Assessments/yourtargetname/Nmap_Result) \n\n")
+            for versions in self.lists2:
+                count1 = count1+1
+                l = str(versions)+"\n".join(" ")
+                cont = f.writelines(l) 
+            cont = f.writelines("\n\n$But for further informations oni uses auxiliary scanners with the help of metasploitable toolkit , so that we can obtain the versions of the ports\n")
+            #print(self.lists2)
             
+    
 
-
+   
+            
 x = summary_manual()
-x.open_file()
-x.summary()
+x.auxiliary()
+#x.open_file()
+#x.nmap()
+#x.summary()
