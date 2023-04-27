f9611370f47eefcf96063d1dd11a1691f4bb748a
KabilPreethamK <kabilpreethamk@gmail.com>
2023-04-27 04:24:45 -0400

    27.04.2023

---
+++ setup.py
@@ -0,0 +1,6 @@
+import os 
+os.system("sudo apt install python3 -y ")
+os.system("sudo apt install python3-pip -y ")
+os.system("sudo apt install gobuster -y ")
+
+os.system("pip3 install -r ./data/requirements.txt")
